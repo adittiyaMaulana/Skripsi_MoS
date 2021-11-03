60bb5ae20f0a5f665b7a23300f84f10e1016891b
AnnaasLuthfi <annaasluthfi@gmail.com>
2021-11-03 21:17:17 +0700

    sidebar

--- resources/views/orangtua/homepage.blade.php
+++ resources/views/orangtua/homepage.blade.php
@@ -6,6 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
     <link rel="stylesheet" href="{{asset('css/sidebar.css')}}">
     <link rel="stylesheet" href="{{asset('css/content.css')}}">
     <title>Sidebar</title>
@@ -28,36 +30,63 @@
             <li class="list">
                 <a href="#">
                     <span class="icon">
-                        <ion-icon name="person-outline"></ion-icon>
+                        <ion-icon name="calendar-outline"></ion-icon>
                     </span>
-                    <span class="title">Profile</span>
+                    <span class="title">jadwal</span>
                 </a>
             </li>
 
             <li class="list">
                 <a href="#">
                     <span class="icon">
-                        <ion-icon name="person-outline"></ion-icon>
+                        <ion-icon name="wallet-outline"></ion-icon>
                     </span>
-                    <span class="title">Message</span>
+                    <span class="title">Finansial</span>
                 </a>
             </li>
 
             <li class="list">
                 <a href="#">
                     <span class="icon">
-                        <ion-icon name="chatbox-outline"></ion-icon>
+                        <ion-icon name="newspaper-outline"></ion-icon>
                     </span>
-                    <span class="title">chat</span>
+                    <span class="title">Berita</span>
                 </a>
             </li>
 
             <li class="list">
                 <a href="#">
                     <span class="icon">
-                        <ion-icon name="settings-outline"></ion-icon>
+                        <ion-icon name="bar-chart-outline"></ion-icon>
                     </span>
-                    <span class="title">Settings</span>
+                    <span class="title">Nilai</span>
+                </a>
+            </li>
+
+            <li class="list">
+                <a href="#">
+                    <span class="icon">
+                        <ion-icon name="create-outline"></ion-icon>
+                    </span>
+                    <span class="title">Kehadiran</span>
+                </a>
+            </li>
+
+            <li class="list">
+                <a href="#">
+                    <span class="icon">
+                        <ion-icon name="download-outline"></ion-icon>
+                    </span>
+                    <span class="title">Fitur Bantuan</span>
+                </a>
+            </li>
+
+            <li class="list">
+                <a href="#">
+                    <span class="icon">
+                        <ion-icon name="alert-circle-outline"></ion-icon>
+                    </span>
+                    <span class="title">Tentang</span>
                 </a>
             </li>
 
@@ -85,9 +114,14 @@
 
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-    <!-- <script src="sidebar.js {{asset('js/sidebar.js')}}" /> -->
-
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+   
     <script>
         // menambahkan active class di select list item (agar bisa di select)
         let list = document.querySelectorAll(".list");
